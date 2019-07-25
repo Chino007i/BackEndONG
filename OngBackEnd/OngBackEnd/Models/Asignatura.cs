03b25fd991e095c071d1aa2c18fa251800d1346a
Daniel <daniel.toves@unitec.edu>
2019-07-25 15:52:37 -0600

    Add files via upload

---
+++ OngBackEnd/OngBackEnd/Models/Asignatura.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngBackEnd.Models
+{
+    public class Asignatura
+    {
+        [Key]
+        public int IdAsignatura { get; set; }
+        public String Asignatur{ get; set; }
+        public int IdGrado { get; set; }
+    }
+}
